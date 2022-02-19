e64b81190ab7fcc4b2f9238a37377aa911edf769
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2022-02-19 01:44:26 +0100

    added addmoney, editmoney & resetmoney (can't use on self)

--- botler/cogs/economy.py
+++ botler/cogs/economy.py
@@ -13,6 +13,8 @@ class Economy(commands.Cog):
         if not member:
             member = ctx.author
         member_balance = await models.Economy.query.where((models.Economy.guild_id == ctx.guild.id) & (models.Economy.member_id == member.id)).gino.first()
+        if (not member_balance):
+            member_balance = await models.Economy.create(guild_id=ctx.guild.id, member_id=member.id)
         embed = discord.Embed(
             title="Balance", timestamp=datetime.datetime.now())
         embed.set_author(name=f"{member.name}#{member.discriminator}",
@@ -20,6 +22,43 @@ class Economy(commands.Cog):
         embed.add_field(
             name="Cash", value=member_balance.balance, inline=False)
         await ctx.reply(embed=embed)
+
+    @commands.command(name='addmoney', aliases=['add-money'])
+    @commands.has_permissions(manage_roles=True)
+    async def _add_money(self, ctx: commands.Context, member: discord.Member, amount: int):
+        member_balance = await models.Economy.query.where((models.Economy.guild_id == ctx.guild.id) & (models.Economy.member_id == member.id)).gino.first()
+        if (member_balance):
+            await member_balance.update(balance=member_balance.balance+amount).apply()
+            new_balance = member_balance.balance
+        else:
+            await models.Economy.create(guild_id=ctx.guild.id, member_id=member.id, balance=amount)
+            member_balance = amount
+        await ctx.reply(
+            f"Added {amount} to {member.name}#{member.discriminator}. His new balance is {new_balance}")
+
+    @commands.command(name='editmoney', aliases=['edit-money', 'setmoney', 'set-money'])
+    @commands.has_permissions(manage_roles=True)
+    async def _edit_money(self, ctx: commands.Context, member: discord.Member, amount: int):
+        member_balance = await models.Economy.query.where((models.Economy.guild_id == ctx.guild.id) & (models.Economy.member_id == member.id)).gino.first()
+        if (member_balance):
+            await member_balance.update(balance=amount).apply()
+        else:
+            await models.Economy.create(guild_id=ctx.guild.id, member_id=member.id, balance=amount)
+        await ctx.reply(
+            f"Set {member.name}#{member.discriminator} balance to {amount}")
+
+    @commands.command(name='resetmoney', aliases=['reset-money'])
+    @commands.has_permissions(manage_roles=True)
+    async def _reset_money(self, ctx: commands.Context, member: discord.Member = None):
+        if not member:
+            member = ctx.author
+        member_balance = await models.Economy.query.where((models.Economy.guild_id == ctx.guild.id) & (models.Economy.member_id == member.id)).gino.first()
+        if (member_balance):
+            await member_balance.update(balance=0).apply()
+        else:
+            await models.Economy.create(guild_id=ctx.guild.id, member_id=member.id)
+        await ctx.reply(
+            f"Resetted {member.name}#{member.discriminator} balance.")
 
 
 def setup(bot):
