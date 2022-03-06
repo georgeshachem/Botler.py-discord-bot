7d3d59b4e983dc9843e49073f2c371e95ecdb164
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2022-03-06 19:11:39 +0100

    added better caching to lirarate fetch

--- botler/cogs/lebanon.py
+++ botler/cogs/lebanon.py
@@ -17,18 +17,32 @@ class Lebanon(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.lirarate_data = None
-        self.current_lirarate_dt = None
+        self.cached = None
+        self.last_fecthed = None
+        self.caching_time_minutes = 5
 
     async def update_lirarate_data(self):
-        self.current_lirarate_dt = datetime.now()
-        current_time = 't{dt.year}{dt.month}{dt.day}{dt.hour}'.format(
-            dt=self.current_lirarate_dt)
-        current_url = LIRARATE_API_URL.format(current_time)
-        async with aiohttp.ClientSession() as session:
-            async with session.get(current_url) as response:
-                if (response.status == 200):
-                    data = await response.json()
-                    self.lirarate_data = data
+        current_dt = datetime.now()
+        if (self.lirarate_data):
+            time_diff = (current_dt -
+                         self.last_fecthed).total_seconds() / 60.0
+        else:
+            time_diff = self.caching_time_minutes + 1
+        if (time_diff > self.caching_time_minutes):
+            current_time = 't{dt.year}{dt.month}{dt.day}{dt.hour}'.format(
+                dt=current_dt)
+            current_url = LIRARATE_API_URL.format(current_time)
+            async with aiohttp.ClientSession() as session:
+                async with session.get(current_url) as response:
+                    if (response.status == 200):
+                        data = await response.json()
+                        self.lirarate_data = data
+                        self.cached = False
+                        self.last_fecthed = current_dt
+                    else:
+                        self.cached = True
+        else:
+            self.cached = True
 
     @commands.command(name='lbprate', aliases=['rate', 'usdrate', 'ratetoday'])
     async def _lbp_rate(self, ctx: commands.Context):
@@ -37,7 +51,7 @@ class Lebanon(commands.Cog):
         buy_data = self.lirarate_data['buy'][-1]
         buy_timestamp, buy_price = buy_data[0], buy_data[1]
         sell_data = self.lirarate_data['sell'][-1]
-        sell_timestamp, sell_price = sell_data[0], sell_data[1]
+        sell_price = sell_data[1]
 
         buy_timestamp = int(buy_timestamp)
         buy_timestamp /= 1000
@@ -45,13 +59,13 @@ class Lebanon(commands.Cog):
             buy_timestamp).astimezone(ZoneInfo('Asia/Beirut'))
         time_text = buy_dt.strftime('%I:%M %p %d/%m/%Y')
 
-        duration = self.current_lirarate_dt - \
+        duration = datetime.now() - \
             datetime.fromtimestamp(buy_timestamp)
         duration_in_s = duration.total_seconds()
         hours = int(duration_in_s//3600)
         minutes = int((duration_in_s % 3600)/60)
 
-        await ctx.reply(f"Buy 1 USD at {buy_price:,} LBP\nSell 1 USD at {sell_price:,} LBP\nUpdated {hours} hours and {minutes} minutes ago at {time_text}")
+        await ctx.reply(f"Buy 1 USD at {buy_price:,} LBP\nSell 1 USD at {sell_price:,} LBP\nUpdated {hours} hours and {minutes} minutes ago at {time_text}\nCached: {self.cached}")
 
     @commands.command(name='convert', aliases=['usdtolbp'])
     async def _convert_usd_to_lbp(self, ctx: commands.Context, amount: int):
