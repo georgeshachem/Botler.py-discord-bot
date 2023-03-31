ef6e4f624b12668300031ed69a6acf1cf22210b3
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2023-04-01 00:09:27 +0200

    check if on windows

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import asyncio
+import os
+
 import botler
 
+if os.name == 'nt':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 asyncio.run(botler.run())
